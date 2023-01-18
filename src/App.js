c1ae37798d64e24cca386ca580f6e9571ba74775
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-01-18 20:43:04 -0300

    argument in function,props to the Square component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ export default function Board() {
 
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  const handleClick = ({target}) => {
+  const handleClick = (i) => {
     
     const nextSquares = squares.slice();
     
-    nextSquares[target] = "X";
+    nextSquares[i] = "X";
     setSquares(nextSquares);
     console.log(squares);
 
@@ -19,19 +19,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick} />
-        <Square value={squares[1]} onSquareClick={handleClick} />
-        <Square value={squares[2]} onSquareClick={handleClick} />
+        <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={()=>handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={handleClick} />
-        <Square value={squares[4]} onSquareClick={handleClick} />
-        <Square value={squares[5]} onSquareClick={handleClick} />
+        <Square value={squares[3]} onSquareClick={()=>handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={()=>handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={()=>handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={handleClick} />
-        <Square value={squares[7]} onSquareClick={handleClick} />
-        <Square value={squares[8]} onSquareClick={handleClick}Ñ />
+        <Square value={squares[6]} onSquareClick={()=>handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={()=>handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={()=>handleClick(8)} />
       </div>
     </>
   );
