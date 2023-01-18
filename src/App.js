17e329a76455ef804af641de438d9459794df550
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-01-18 20:46:35 -0300

    conditional square render

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import Square from "./components/Square";
 
 export default function Board() {
 
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   const handleClick = (i) => {
     
     const nextSquares = squares.slice();
     
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
-    console.log(squares);
+    setXIsNext(!xIsNext);
 
   }
 
