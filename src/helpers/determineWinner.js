ba602f5b5e0a8279b5c6e988a0fd30eb2eff6f33
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-01-22 12:54:51 -0300

    declare a winner

--- src/helpers/determineWinner.js
+++ src/helpers/determineWinner.js
@@ -2,6 +2,7 @@
 
 
 export const determineWinner = (squares) => {
+
     const lines = [
         [0, 1, 2],
         [3, 4, 5],
@@ -12,6 +13,7 @@ export const determineWinner = (squares) => {
         [0, 4, 8],
         [2, 4, 6]
     ];
+    
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
